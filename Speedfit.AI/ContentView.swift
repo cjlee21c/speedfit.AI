49023b598000ee58a02a403c7567d008a9ff025a
cjlee21c <cjlee21c@bu.edu>
2025-07-25 17:54:35 +0900

    Initial Commit

---
+++ Speedfit.AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speedfit.AI
+//
+//  Created by 이찬주 on 7/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speedfit.AI/Speedfit_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speedfit_AIApp.swift
+//  Speedfit.AI
+//
+//  Created by 이찬주 on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Speedfit_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
